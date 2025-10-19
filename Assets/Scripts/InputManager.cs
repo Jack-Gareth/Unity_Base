679710ef3f82878516af3f2fc9cb29768e593bbc
Macintache <neoncoffee@outlook.com>
2025-10-19 23:01:35 +0100

    Moved Red Level Mechanic to key press [G]. Interval set to 1 second per activation. Additionally, lightnened up level colours to be less taxing on the eyes

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class GameInputManager : MonoBehaviour
 {
     public static GameInputManager Instance { get; private set; }
-    
+
     [Header("Input Events")]
     public System.Action<Vector2> OnMoveInput;
     public System.Action OnJumpInput;
@@ -15,9 +15,10 @@ public class GameInputManager : MonoBehaviour
     public System.Action OnPinkColorInput;
     public System.Action OnBrownColorInput;
     public System.Action OnGravityFlipInput;
-    
+    public System.Action OnRedPhaseAbilityInput;
+
     private InputSystem_Actions playerInputActions;
-    
+
     private void Awake()
     {
         if (Instance == null)
@@ -31,11 +32,11 @@ public class GameInputManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    // who
+
     private void SetupInputActions()
     {
         playerInputActions = new InputSystem_Actions();
-        
+
         playerInputActions.Player.Move.performed += OnMovePerformed;
         playerInputActions.Player.Move.canceled += OnMoveCanceled;
         playerInputActions.Player.Jump.performed += OnJumpPerformed;
@@ -46,18 +47,19 @@ public class GameInputManager : MonoBehaviour
         playerInputActions.Player.PinkColor.performed += OnPinkColorPerformed;
         playerInputActions.Player.BrownColor.performed += OnBrownColorPerformed;
         playerInputActions.Player.GravityFlip.performed += OnGravityFlipPerformed;
+        playerInputActions.Player.RedPhaseAbility.performed += OnRedPhaseAbilityPerformed;
     }
-    
+
     private void OnEnable()
     {
         playerInputActions?.Enable();
     }
-    
+
     private void OnDisable()
     {
         playerInputActions?.Disable();
     }
-    
+
     private void OnDestroy()
     {
         if (playerInputActions != null)
@@ -72,59 +74,65 @@ public class GameInputManager : MonoBehaviour
             playerInputActions.Player.PinkColor.performed -= OnPinkColorPerformed;
             playerInputActions.Player.BrownColor.performed -= OnBrownColorPerformed;
             playerInputActions.Player.GravityFlip.performed -= OnGravityFlipPerformed;
+            playerInputActions.Player.RedPhaseAbility.performed -= OnRedPhaseAbilityPerformed;
             playerInputActions.Dispose();
         }
     }
-    
+
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
         OnMoveInput?.Invoke(context.ReadValue<Vector2>());
     }
-    
+
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
         OnMoveInput?.Invoke(Vector2.zero);
     }
-    
+
     private void OnJumpPerformed(InputAction.CallbackContext context)
     {
         OnJumpInput?.Invoke();
     }
-    
+
     private void OnRedColorPerformed(InputAction.CallbackContext context)
     {
         OnRedColorInput?.Invoke();
     }
-    
+
     private void OnBlueColorPerformed(InputAction.CallbackContext context)
     {
         OnBlueColorInput?.Invoke();
     }
-    
+
     private void OnGreenColorPerformed(InputAction.CallbackContext context)
     {
         OnGreenColorInput?.Invoke();
     }
-    
+
     private void OnYellowColorPerformed(InputAction.CallbackContext context)
     {
         OnYellowColorInput?.Invoke();
     }
-    
+
     private void OnPinkColorPerformed(InputAction.CallbackContext context)
     {
         OnPinkColorInput?.Invoke();
     }
-    
+
     private void OnBrownColorPerformed(InputAction.CallbackContext context)
     {
         OnBrownColorInput?.Invoke();
     }
-    
+
     private void OnGravityFlipPerformed(InputAction.CallbackContext context)
     {
         Debug.Log("GameInputManager: OnGravityFlipPerformed called - H key pressed!");
         OnGravityFlipInput?.Invoke();
         PlayerEvents.TriggerGravityFlip();
+    }
+
+    private void OnRedPhaseAbilityPerformed(InputAction.CallbackContext context)
+    {
+        OnRedPhaseAbilityInput?.Invoke();
     }
 }
