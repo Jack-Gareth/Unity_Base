d47468ce47fca2cabec4408835ec4043e60139e6
spritesnbytes <85615010+spritesnbytes@users.noreply.github.com>
2025-11-25 15:19:53 +0000

    Added a checkbox in the LevelMechanics gameObject that sets View All LevelMechanics gameObjects in the scene.

--- Assets/Scripts/Editor/LevelMechanicsVisualizer.cs
+++ Assets/Scripts/Editor/LevelMechanicsVisualizer.cs
@@ -1,44 +1,134 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(LevelMechanicsManager))]
 public class LevelMechanicsVisualizer : Editor
 {
     private const string VISUALIZER_CHILD_NAME = "_EditorVisualization";
+    private const string SHOW_ALL_PREF_KEY = "LevelMechanicsVisualizer_ShowAll";
+    
+    private static bool showAllMechanics = false;
+    private static Dictionary<LevelMechanicsManager, GameObject> allVisualizers = new Dictionary<LevelMechanicsManager, GameObject>();
+
+    private void OnEnable()
+    {
+        showAllMechanics = EditorPrefs.GetBool(SHOW_ALL_PREF_KEY, false);
+        SceneView.duringSceneGui += OnSceneGUIGlobal;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnSceneGUIGlobal;
+        CleanupVisualizer((LevelMechanicsManager)target);
+    }
 
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
 
-        LevelMechanicsManager manager = (LevelMechanicsManager)target;
-        UpdateVisualization(manager);
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Scene Visualization", EditorStyles.boldLabel);
+        
+        EditorGUI.BeginChangeCheck();
+        showAllMechanics = EditorGUILayout.Toggle("Show All Level Mechanics", showAllMechanics);
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorPrefs.SetBool(SHOW_ALL_PREF_KEY, showAllMechanics);
+            
+            if (showAllMechanics)
+            {
+                UpdateAllVisualizations();
+            }
+            else
+            {
+                CleanupAllVisualizers();
+            }
+            
+            SceneView.RepaintAll();
+        }
+
+        if (showAllMechanics)
+        {
+            EditorGUILayout.HelpBox("Showing all Level Mechanics in the scene. Uncheck to show only the selected one.", MessageType.Info);
+        }
+    }
+
+    private void OnSceneGUIGlobal(SceneView sceneView)
+    {
+        if (showAllMechanics)
+        {
+            UpdateAllVisualizations();
+        }
     }
 
     private void OnSceneGUI()
     {
-        LevelMechanicsManager manager = (LevelMechanicsManager)target;
-        UpdateVisualization(manager);
+        if (!showAllMechanics)
+        {
+            LevelMechanicsManager manager = (LevelMechanicsManager)target;
+            UpdateSingleVisualization(manager);
+        }
     }
 
-    private void UpdateVisualization(LevelMechanicsManager manager)
+    private void UpdateAllVisualizations()
+    {
+        LevelMechanicsManager[] allManagers = FindObjectsByType<LevelMechanicsManager>(FindObjectsSortMode.None);
+        
+        HashSet<LevelMechanicsManager> currentManagers = new HashSet<LevelMechanicsManager>(allManagers);
+        List<LevelMechanicsManager> toRemove = new List<LevelMechanicsManager>();
+        
+        foreach (var kvp in allVisualizers)
+        {
+            if (kvp.Key == null || !currentManagers.Contains(kvp.Key))
+            {
+                toRemove.Add(kvp.Key);
+            }
+        }
+        
+        foreach (var manager in toRemove)
+        {
+            CleanupVisualizer(manager);
+        }
+
+        foreach (LevelMechanicsManager manager in allManagers)
+        {
+            if (manager != null)
+            {
+                UpdateSingleVisualization(manager);
+            }
+        }
+    }
+
+    private void UpdateSingleVisualization(LevelMechanicsManager manager)
     {
         if (manager == null) return;
 
-        Transform visualizerTransform = manager.transform.Find(VISUALIZER_CHILD_NAME);
         GameObject visualizerObject;
-
-        if (visualizerTransform == null)
+        
+        if (allVisualizers.ContainsKey(manager) && allVisualizers[manager] != null)
         {
-            visualizerObject = new GameObject(VISUALIZER_CHILD_NAME);
-            visualizerObject.transform.SetParent(manager.transform);
-            visualizerObject.transform.localPosition = Vector3.zero;
-            visualizerObject.transform.localRotation = Quaternion.identity;
-            visualizerObject.transform.localScale = Vector3.one;
-            visualizerObject.hideFlags = HideFlags.HideInHierarchy | HideFlags.DontSave;
+            visualizerObject = allVisualizers[manager];
         }
         else
         {
-            visualizerObject = visualizerTransform.gameObject;
+            Transform visualizerTransform = manager.transform.Find(VISUALIZER_CHILD_NAME);
+            
+            if (visualizerTransform == null)
+            {
+                visualizerObject = new GameObject(VISUALIZER_CHILD_NAME);
+                visualizerObject.transform.SetParent(manager.transform);
+                visualizerObject.transform.localPosition = Vector3.zero;
+                visualizerObject.transform.localRotation = Quaternion.identity;
+                visualizerObject.transform.localScale = Vector3.one;
+                visualizerObject.hideFlags = HideFlags.HideInHierarchy | HideFlags.DontSave;
+            }
+            else
+            {
+                visualizerObject = visualizerTransform.gameObject;
+            }
+            
+            allVisualizers[manager] = visualizerObject;
         }
 
         SpriteRenderer spriteRenderer = visualizerObject.GetComponent<SpriteRenderer>();
@@ -103,15 +193,58 @@ public class LevelMechanicsVisualizer : Editor
         return Sprite.Create(texture, new Rect(0, 0, 1, 1), new Vector2(0.5f, 0.5f), 1);
     }
 
-    private void OnDisable()
+    private void CleanupVisualizer(LevelMechanicsManager manager)
     {
-        LevelMechanicsManager manager = (LevelMechanicsManager)target;
         if (manager != null)
         {
             Transform visualizerTransform = manager.transform.Find(VISUALIZER_CHILD_NAME);
             if (visualizerTransform != null)
             {
                 DestroyImmediate(visualizerTransform.gameObject);
+            }
+        }
+        
+        if (allVisualizers.ContainsKey(manager))
+        {
+            if (allVisualizers[manager] != null)
+            {
+                DestroyImmediate(allVisualizers[manager]);
+            }
+            allVisualizers.Remove(manager);
+        }
+    }
+
+    private void CleanupAllVisualizers()
+    {
+        List<LevelMechanicsManager> keys = new List<LevelMechanicsManager>(allVisualizers.Keys);
+        foreach (var manager in keys)
+        {
+            CleanupVisualizer(manager);
+        }
+        allVisualizers.Clear();
+    }
+
+    [InitializeOnLoadMethod]
+    private static void Initialize()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode || state == PlayModeStateChange.ExitingPlayMode)
+        {
+            LevelMechanicsManager[] allManagers = FindObjectsByType<LevelMechanicsManager>(FindObjectsSortMode.None);
+            foreach (var manager in allManagers)
+            {
+                if (manager != null)
+                {
+                    Transform visualizerTransform = manager.transform.Find(VISUALIZER_CHILD_NAME);
+                    if (visualizerTransform != null)
+                    {
+                        DestroyImmediate(visualizerTransform.gameObject);
+                    }
+                }
             }
         }
     }
